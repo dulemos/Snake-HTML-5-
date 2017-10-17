263409634970d282ae93edafd35d1322d86c3332
dulemos <32346249+dulemos@users.noreply.github.com>
2017-10-17 20:05:14 -0200

    primeiraTentativa

---
+++ js/script.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+	jogador();
+}
+
+function jogador(){
+	var canvas = document.getElementById("teste");
+	var context = canvas.getContext("2d");
+	context.fillRect(195, 585, 90, 15);
+	
+}
+
